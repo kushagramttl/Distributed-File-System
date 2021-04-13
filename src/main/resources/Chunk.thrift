d68f6d4029709f3df53955ecf8c35d0a737dc291
Soufiane Jounaid <soufianej@pop-os.localdomain>
2021-04-13 18:36:31 -0400

    Generated interfaces for filesystem access and chunk access

--- src/main/resources/Chunk.thrift
+++ src/main/resources/Chunk.thrift
@@ -1,10 +1,10 @@
 typedef i32 int
 
 service Chunk{
-    void uploadFile(),
-    bool containsFile(),
-    void updateFile(),
-    void deleteFile(),
-    void getFile(),
-    void getMetadateTable(),
+    binary getFile(1:string name),
+    bool containsFile(1:string name),
+    void deleteFile(1:string name),
+    binary getMetadata(),
+    void uploadFile(1:string name, 2:binary file),
+    void updateFile(1:string name, 2:binary file),
 }
--- src/main/resources/FileSystem.thrift
+++ src/main/resources/FileSystem.thrift
@@ -1,8 +1,8 @@
 typedef i32 int
 
 service FileSystem{
-    void uploadFile(),
-    void updateFile(),
-    void deleteFile(),
-    void getFile(),
+    binary getFile(1:string name),
+    void uploadFile(1:string name, 2:binary file),
+    void updateFile(1:string name, 2:binary file),
+    void deleteFile(1:string name),
 }
